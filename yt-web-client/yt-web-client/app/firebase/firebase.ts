892d16aa13ca24716d42afac1b636a83b052c047
Ridham Khandar <ridhamk.j@gmail.com>
2025-07-26 09:47:24 +0530

    Update firebase.ts

--- yt-web-client/yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/yt-web-client/app/firebase/firebase.ts
@@ -8,15 +8,12 @@ import {
   User
  } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC0yKzuImmCCWlS0UQjIyhis9mSgXwXX40",
-  authDomain: "yt-clone-f85f5.firebaseapp.com",
-  projectId: "yt-clone-f85f5",
-  appId: "1:370848107359:web:a9c601b08608c0677a84ea",
-  measurementId: "G-65H3R0MNRM"
+  apiKey: api-key,
+  authDomain: auth-domain,
+  projectId: project-id,
+  appId: app-id,
+  measurementId: m-id
 };
 
 // Initialize Firebase
